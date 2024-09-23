4688ddbe1f6f2f6923a295ffdfb42f9cf77a9b38
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-09-23 16:01:21 +0100

    cli utility for user command

--- src/tresa_graph_api/check_all_users.py
+++ src/tresa_graph_api/check_all_users.py
@@ -8,34 +8,40 @@ Usage: `python check_all_users.py`
 You will be prompted to open a browser and authenticate using your Admin credentials.
 """
 
-
-from azure.identity import DeviceCodeCredential
-from msgraph import GraphServiceClient
-import asyncio
 import argparse
+import asyncio
 import sys
 
+from azure.identity import DeviceCodeCredential  # type: ignore[import-not-found]
+from msgraph import GraphServiceClient  # type: ignore[import-not-found]
 
 credentials = DeviceCodeCredential()
-scopes = ['https://graph.microsoft.com/.default']
+scopes = ["https://graph.microsoft.com/.default"]
 
 client = GraphServiceClient(credentials=credentials, scopes=scopes)
 
 
 def cli(args=None):
-
     if not args:
         args = sys.argv[1:]
 
-    parser = argparse.ArgumentParser(description="Get user data from Microsoft Entra ID using Microsoft Graph API.")
+    parser = argparse.ArgumentParser(
+        description="Get user data from Microsoft Entra ID using Microsoft Graph API."
+    )
     subparsers = parser.add_subparsers(dest="command")
 
     user_parser = subparsers.add_parser("user", help="Get user data.")
     user_parser.add_argument("--id", type=str, help="The user ID to get data for.")
     user_parser.add_argument("--name", type=str, help="The user name to get data for.")
-    user_parser.add_argument("--email", type=str, help="The user email to get data for.")
-    user_parser.add_argument("--admins", action="store_true", help="Get data for all admins.")
-    user_parser.add_argument("--unassigned", action="store_true", help="Get data for all unassigned users.")
+    user_parser.add_argument(
+        "--email", type=str, help="The user email to get data for."
+    )
+    user_parser.add_argument(
+        "--admins", action="store_true", help="Get data for all admins."
+    )
+    user_parser.add_argument(
+        "--unassigned", action="store_true", help="Get data for all unassigned users."
+    )
 
     subparsers.add_parser("group", help="Get group data.")
 
@@ -51,8 +57,7 @@ async def get_users():
 async def get_user_roles(user_id):
     roles = await client.users.by_user_id(user_id).member_of.get()
     roles = roles.value
-    roles = [role.display_name for role in roles]
-    return roles
+    return [role.display_name for role in roles]
 
 
 async def get_groups():
@@ -71,15 +76,87 @@ async def main(args):
     users = await get_users()
     groups = await get_groups()
 
-    print("User ID, Display Name, Email, Member Of, Roles")
-    print("----------------------------------------------")
-    for user in users:
-        member_of = []
-        roles = await get_user_roles(user.id)
-        for name, members in groups.items():
-            if user.id in members:
-                member_of.append(name)
-        print(user.id, ",", user.display_name, ",", user.mail, ",", member_of, ",", roles)
+    if args.command == "user":
+        if args.id:
+            fetched_users = [user for user in users if user.id == args.id]
+        elif args.name:
+            fetched_users = [user for user in users if args.name in user.display_name]
+        elif args.email:
+            fetched_users = [user for user in users if user.mail == args.email]
+        elif args.admins or args.unassigned:
+            fetched_users = users
+
+        print("User ID, Display Name, Email, Member of, Is Global Admin")
+        print("--------------------------------------------------------")
+
+        if fetched_users and not (args.admins or args.unassigned):
+            for user in fetched_users:
+                member_of = []
+                roles = await get_user_roles(user.id)
+                is_admin = "Global Administrator" in roles
+                for group_name, members in groups.items():
+                    if user.id in members:
+                        member_of.append(group_name)
+                print(
+                    user.id,
+                    ",",
+                    user.display_name,
+                    ",",
+                    user.mail,
+                    ",",
+                    member_of,
+                    ",",
+                    is_admin,
+                )
+                print("\n")
+        elif fetched_users and args.admins:
+            for user in fetched_users:
+                roles = await get_user_roles(user.id)
+                is_admin = "Global Administrator" in roles
+                if is_admin:
+                    member_of = []
+                    for group_name, members in groups.items():
+                        if user.id in members:
+                            member_of.append(group_name)
+                    print(
+                        user.id,
+                        ",",
+                        user.display_name,
+                        ",",
+                        user.mail,
+                        ",",
+                        member_of,
+                        ",",
+                        is_admin,
+                    )
+                    print("\n")
+        elif fetched_users and args.unassigned:
+            for user in fetched_users:
+                is_member = False
+                for _, members in groups.items():
+                    if user.id in members:
+                        is_member = True
+                        break
+                if not is_member:
+                    roles = await get_user_roles(user.id)
+                    is_admin = "Global Administrator" in roles
+                    print(
+                        user.id,
+                        ",",
+                        user.display_name,
+                        ",",
+                        user.mail,
+                        ",",
+                        [],
+                        ",",
+                        is_admin,
+                    )
+                    print("\n")
+        else:
+            print("No users found for the given criteria.")
+
+    elif args.command == "group":
+        print("Requested group data")
 
 
 if __name__ == "__main__":
